6a1aeca0fef6b42c556de2eb5247057bc54d0292
omar_hussein <omar_hussein201014@hotmail.com>
2022-12-07 00:19:02 +0200

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'reactiveForm-assignment';
+  projectForm:FormGroup;
+
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null ,[Validators.required]),
+      'userMail':new FormControl(null ,[Validators.required ,Validators.email]),
+      'projectStatus':new FormControl('critical')
+    })
+  }
+
+  onSubmit(){
+    console.log(this.projectForm);
+  }
+
 }
